5622e902a2c68252af74b1ab3dd1908ab9d7322d
divakaivan <timee98@outlook.com>
2024-07-17 01:12:14 +0900

    [db] create table in postgres

--- postgres/create_table.sql
+++ postgres/create_table.sql
@@ -1,6 +1,14 @@
-CREATE TABLE sentences (
-    id SERIAL PRIMARY KEY,
-    sentence VARCHAR(10000) NOT NULL,
-    sentiment FLOAT
-    );
-)
+CREATE TABLE IF NOT EXISTS transactions (
+    transaction_id VARCHAR(255) PRIMARY KEY,
+    amount INTEGER,
+    currency VARCHAR(10),
+    created TIMESTAMP,
+    network_status VARCHAR(50),
+    risk_level VARCHAR(50),
+    risk_score INTEGER,
+    seller_message TEXT,
+    paid BOOLEAN,
+    card_brand VARCHAR(50),
+    receipt_number VARCHAR(50),
+    receipt_url TEXT
+);
